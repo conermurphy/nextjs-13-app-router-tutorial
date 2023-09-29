f8316a4099b618b52b8e86023da843f9447c9e21
Coner Murphy <coner@conermurphy.com>
2023-09-29 18:15:24 +0100

    chore: updated background colors

--- app/(group)/about/page.tsx
+++ app/(group)/about/page.tsx
@@ -1,6 +1,6 @@
 export default function Page() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24 ">
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="text-black">
         <p>About</p>
       </div>
