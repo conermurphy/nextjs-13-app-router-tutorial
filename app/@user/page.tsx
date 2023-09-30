17c1e4440a1d8e42f6880baf5ac9e16fecb84891
Coner Murphy <coner@conermurphy.com>
2023-09-30 09:25:20 +0100

    feat: added parallel routes

---
+++ app/@user/page.tsx
@@ -0,0 +1,9 @@
+export default function Page() {
+  return (
+    <main className="flex flex-col items-center justify-between p-24 bg-green-300">
+      <div className="text-black">
+        <p>User</p>
+      </div>
+    </main>
+  );
+}
